d562e82d8fc354f2c0fb7526c214682c6f011fa6
David Towson <david115632042@gmail.com>
2016-10-31 12:09:15 -0400

    init commit

---
+++ todoList.Tests/Main.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity.Core.Objects;
+
+
+using Xunit;
+using Xunit.Abstractions;
+
+using todolist.DataAccess;
+
+namespace todoList.Tests
+{
+    public class Main
+    {
+
+        private readonly ITestOutputHelper output;
+        private Access access;
+
+        public Main(ITestOutputHelper _output)
+        {
+            this.output = _output;
+            this.access = new Access();
+        }
+
+        [Fact]
+        public void testInsert()
+        {
+            List<getItems_Result> items = access.getItems();
+            int initialCount = items.Count();
+
+            int rows = access.insertItem("here's a reminder to do something", false);
+            items = access.getItems();
+            int finalCount = items.Count();
+
+            Assert.Equal(initialCount + 1, finalCount);
+            Assert.Equal(rows, 1);
+
+            output.WriteLine("items: " + items.Count().ToString());
+            output.WriteLine("rows affected: " + rows);
+
+        }
+
+        [Fact]
+        public void testUpdateCompleteness()
+        {
+            List<getItems_Result> items = access.getItems();
+
+            int id = items.First<getItems_Result>().id;
+
+            access.updateItemCompleteness(id, true);
+
+            items = access.getItems();
+            Assert.Equal(items.Where(a => a.id == id).First().isComplete, true);
+
+            output.WriteLine(items.Count().ToString());
+
+        }
+
+        [Fact]
+        public void testUpdateText()
+        {
+            List<getItems_Result> items = access.getItems();
+            int id = items.First<getItems_Result>().id;
+            access.updateItemCompleteness(id, true);
+            items = access.getItems();
+            output.WriteLine(items.Count().ToString());
+
+        }
+
+        [Fact]
+        public void testRemoveItem()
+        {
+            List<getItems_Result> items = access.getItems();
+            int id = items.First<getItems_Result>().id;
+            access.removeItem(id);
+            items = access.getItems();
+            output.WriteLine(items.Count().ToString());
+
+        }
+
+        [Fact]
+        public void testGetItems()
+        {
+            List<getItems_Result> items = access.getItems();
+        }
+
+        [Fact]
+        public void testGetItemsComplete()
+        {
+            List<getItemsComplete_Result> items = access.getItemsComplete();
+        }
+
+        [Fact]
+        public void testGetItemsIncomplete()
+        {
+            List<getItemsIncomplete_Result> items = access.getItemsIncomplete();
+        }
+
+
+    }
+}
---
+++ todolist.DataAccess/Access.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todolist.DataAccess
+{
+    public class Access
+    {
+
+        private todolistEntities entities;
+
+        public Access()
+        {
+            this.entities = new todolistEntities();
+        }
+
+        public int insertItem(string text, bool complete)
+        {
+            int rows = entities.insertItem(text, complete);
+            entities.SaveChanges();
+            return rows;
+            
+        }
+
+        public void updateItemText(int id, string text)
+        {
+            entities.updateItemText(id, text);
+            entities.SaveChanges();
+        }
+
+        public void updateItemCompleteness(int id, bool isComplete)
+        {
+            entities.updateItemCompleteness(id, isComplete);
+            entities.SaveChanges();
+        }
+
+        public void removeItem(int id)
+        {
+            entities.removeItem(id);
+            entities.SaveChanges();
+        }
+
+        public List<getItems_Result> getItems()
+        {
+            return entities.getItems().ToList();
+            
+        }
+
+        public List<getItemsComplete_Result> getItemsComplete()
+        {
+            return entities.getItemsComplete().ToList();
+
+        }
+
+        public List<getItemsIncomplete_Result> getItemsIncomplete()
+        {
+            return entities.getItemsIncomplete().ToList();
+
+        }
+
+    }
+}
